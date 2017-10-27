1e4db78590fc73fb023f4ec2e837b96aa58aafbf
Ryan Tate <ryan.michael.tate@gmail.com>
2017-10-27 12:30:42 -0700

    Remove default values

--- src/reducers/register.js
+++ src/reducers/register.js
@@ -1,24 +1,13 @@
-
-
 const DEFAULT_STATE = {
-  organization: 'git-token',
-  adminAddress: '0x48299b423342db084b49d97c8b09392f6156cdc7',
-  adminUsername: 'Ryanmtate',
-  authToken: '82720e8ae222f2d266cda80a707eaf1056189517',
+  organization: '',
+  adminAddress: '0x0',
+  adminUsername: '',
+  authToken: '',
   decimals: 8,
-  symbol: 'GTK',
-  tokenName: 'GitToken',
+  symbol: '',
+  tokenName: '',
   registered: false,
-  showRegistration: false
-  // organization: '',
-  // adminAddress: '0x0',
-  // adminUsername: '',
-  // authToken: '',
-  // decimals: 8,
-  // symbol: '',
-  // tokenName: '',
-  // registered: false,
-  // showRegistration: true
+  showRegistration: true
 }
 
 export default function register(state=DEFAULT_STATE, action) {
