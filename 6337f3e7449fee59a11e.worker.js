fd180c6f7a2a6d99b3026329ddec952f53bf7370
Ryan Tate <ryan.michael.tate@gmail.com>
2017-10-27 12:16:35 -0700

    Build src

---
+++ 6337f3e7449fee59a11e.worker.js
@@ -0,0 +1,132 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _objectDestructuringEmpty(obj) { if (obj == null) throw new TypeError("Cannot destructure undefined"); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var GitTokenRegistryWorker = function () {
+  function GitTokenRegistryWorker(_ref) {
+    _objectDestructuringEmpty(_ref);
+
+    _classCallCheck(this, GitTokenRegistryWorker);
+
+    this.listen();
+  }
+
+  _createClass(GitTokenRegistryWorker, [{
+    key: 'listen',
+    value: function listen() {
+      var _this = this;
+
+      console.log('GitToken Registry Web Worker Listening for Events');
+      addEventListener('message', function (msg) {
+        var _JSON$parse = JSON.parse(msg.data),
+            event = _JSON$parse.event,
+            values = _JSON$parse.values;
+
+        switch (event) {
+          default:
+            _this.handleErrorMessage({
+              error: 'Invalid Event: ' + event
+            });
+        }
+      });
+    }
+  }, {
+    key: 'handleErrorMessage',
+    value: function handleErrorMessage(_ref2) {
+      var error = _ref2.error;
+
+      postMessage(JSON.stringify({
+        error: error ? error : 'Unhandled Error'
+      }));
+    }
+  }]);
+
+  return GitTokenRegistryWorker;
+}();
+
+exports.default = GitTokenRegistryWorker;
+
+
+var worker = new GitTokenRegistryWorker({});
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=6337f3e7449fee59a11e.worker.js.map
